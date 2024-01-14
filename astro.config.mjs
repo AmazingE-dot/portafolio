a1b69c0a3a647eff773a74fbec1edb16dd6bbbf2
EduDev <82557141+AmazingE-dot@users.noreply.github.com>
2024-01-14 18:53:13 -0500

    Arreglos varios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://AmazingE-dot.github.io',
+  base: '/portafolio',
   integrations: [tailwind()]
 });
